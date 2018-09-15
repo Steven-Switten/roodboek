7b00bf5603b4390674a7a3ddb8c954ffee747f3d
Steven <switten@gmail.com>
2018-09-15 10:35:00 +0200

    ng commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyASKLOJ3yw03ud7HMcr6ZbmZ-H02DvEmN8",
+    authDomain: "roodboek-d3682.firebaseapp.com",
+    databaseURL: "https://roodboek-d3682.firebaseio.com",
+    projectId: "roodboek-d3682",
+    storageBucket: "roodboek-d3682.appspot.com",
+    messagingSenderId: "682799586486"
+  }
+};
