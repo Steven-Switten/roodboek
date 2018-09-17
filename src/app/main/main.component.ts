68f8ce1661f09e82932357d1fb593068df0ce29b
Steven <switten@gmail.com>
2018-09-17 06:58:02 +0200

    main page updated

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,9 +10,34 @@ export class MainComponent implements OnInit {
   mainTitle = 'Ons Roodboek';
   subTitle = 'Naar links en dan vooruit!';
   bottomTitle = 'Ons Roodboek voor een betere en eerlijkere maatschappij';
+  bottomText = this.loremIpsum();
+
   faChevronDown = faChevronDown;
   fragment: string;
-  constructor() {}
+  tiles: any[] = [];
+  constructor() {
+    this.tiles.push({ text: 'Hoofdstuk 1', color: 'blue' });
+    this.tiles.push({ text: 'Hoofdstuk 2', color: 'red' });
+    this.tiles.push({ text: 'Hoofdstuk 3', color: 'green' });
+    this.tiles.push({ text: 'Hoofdstuk 4', color: 'yellow' });
+    this.tiles.push({ text: 'Hoofdstuk 5', color: 'brown' });
+    this.tiles.push({ text: 'Hoofdstuk 6', color: 'grey' });
+    this.tiles.push({ text: 'Hoofdstuk 7', color: 'orange' });
+    this.tiles.push({ text: 'Hoofdstuk 8', color: 'pink' });
+    this.tiles.push({ text: 'Hoofdstuk 9', color: 'teal' });
+    this.tiles.push({ text: 'Hoofdstuk 10', color: 'purple' });
+    this.tiles.push({ text: 'Hoofdstuk 11', color: 'red' });
+    this.tiles.push({ text: 'Hoofdstuk 12', color: 'green' });
+  }
 
   ngOnInit() {}
+
+  loremIpsum(): string {
+    return `
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas libero. Nullam eget nisl. Integer tempor. Etiam dictum tincidunt diam. Sed convallis magna eu sem. In convallis. 
+
+Nunc auctor. In dapibus augue non sapien. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Suspendisse sagittis ultrices augue. Aliquam erat volutpat. Aenean placerat. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo.
+
+Curabitur bibendum justo non orci. Praesent in mauris eu tortor porttitor accumsan. Fusce wisi. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. `;
+  }
 }
